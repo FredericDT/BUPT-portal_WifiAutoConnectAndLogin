6b89dd0a99b88013af17d7cd49670139b8049c84
FredericDT <i@fdt.onl>
2018-01-05 13:30:57 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,19 +5,19 @@ Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" "AutoRunW
 #writeRegInfo
 Out-File -FilePath OnStart.cmd -InputObject ('@echo off && %SystemRoot%\system32\WindowsPowerShell\v1.0\powershell -ExecutionPolicy Unrestricted "'+$workpath+'AutoLogin.ps1'+'" && exit') -Encoding ascii
 #OnStart.cmd
-'#author FredericDT
+('#author FredericDT
 #contact i@fdt.onl
 
 netsh wlan connect ssid=BUPT-portal name=BUPT-portal
 echo "Please wait for 5 seconds"
 sleep 5
-$config=Get-Content config.txt
+$config=Get-Content '+$workpath+'config.txt
 $un = $config[0]
 $pw = $config[1]
 $f = @{DDDDD=$un;upass=$pw;"0MKKey"=""}
 $R=Invoke-WebRequest -Uri "http://10.3.8.211/" -Method POST -Body $f
 echo "Task complete"
-'>AutoLogin.ps1
+')>AutoLogin.ps1
 #AutoLogin.ps1
 ''>config.txt
 #config.txt
